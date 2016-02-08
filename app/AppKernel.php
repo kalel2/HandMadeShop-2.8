37f814539d8a8d0c6ed4ec136a383bd196fa0b35
Sehiy Hrynenko <serhiy@inuits.eu>
2016-02-08 17:37:14 +0200

    start implement multilanguages

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,8 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
+            new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
